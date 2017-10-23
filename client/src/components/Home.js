13338a4d5178bdc66e805b79aeba00143e01c34c
Merty McGraw <mertymcgraw@gmail.com>
2017-10-23 12:10:47 -0700

    add homepage video, logo, css, and routing

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router,
+  Route,
+  Link} from 'react-router-dom'
 import '../styles/App.css';
 
 class Home extends Component {
+
+
   render (){
     return(
-      <h1>Home Page</h1>
+      <div>
+        <div id="vid-section">
+         <video id="bgvideo" preload="auto" autoPlay="true" loop="loop" muted="muted">
+          <source src="manta.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+
+      <div className="logo-section">
+        <img src="white-cs-logo.png"/>
+        <Link to="/divelog" className="entry-button">Log Book</Link>
+      </div>
+
+    </div>
       )
   }
 }
